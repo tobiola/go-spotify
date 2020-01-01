9bce5f580ff473a4adacc6cbd3bce37901c13656
Tobi Ola <oluwatobi.ola@gmail.com>
2019-12-31 19:56:30 -0500

    uploaded previous code

---
+++ player.go
@@ -0,0 +1,59 @@
+package spotify
+
+import (
+	"time"
+	"strings"
+	"fmt"
+	"net/http"
+)
+
+// Start/Resume a User's Playback
+func Play(accessToken string) error {
+	client := &http.Client{Timeout: time.Second * 5}
+	request, err := http.NewRequest("PUT", "https://api.spotify.com/v1/me/player/play", nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Start/Resume a User's Playback
+func PlayTrack(accessToken string, track Track) error {
+	client := &http.Client{Timeout: time.Second * 5}
+	request, err := http.NewRequest("PUT", "https://api.spotify.com/v1/me/player/play", strings.NewReader(fmt.Sprintf(`{"uris":["%s"]}`, track.Uri)))
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Pause a User's Playback
+func Pause(accessToken string) error {
+	client := &http.Client{Timeout: time.Second * 5}
+	request, err := http.NewRequest("PUT", "https://api.spotify.com/v1/me/player/pause", nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ search.go
@@ -0,0 +1,43 @@
+package spotify
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"io/ioutil"
+	"net/url"
+	"net/http"
+	"encoding/json"
+)
+
+// Search for an item
+func Search(accessToken string, query string) (SearchResult, error) {
+	params := make(url.Values)
+	params.Add("q", query)
+	params.Add("type", "track")
+	params.Add("limit", "7")
+	
+	url := "https://api.spotify.com/v1/search?" + params.Encode()
+	client := &http.Client{Timeout: time.Second * 5}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	result := SearchResult{}
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
